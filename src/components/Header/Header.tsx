00799dd4c4fe6c020582af473ca563969c88f12a
dyi919 <ehddjs13579@gmail.com>
2023-02-08 00:10:56 +0900

    [style] add border around Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import HeaderButton from './HeaderButton';
 
 const Header = () => {
   return (
-    <div className="flex justify-between border-gray-900 px-16 py-8">
+    <div className="flex justify-between border-2 border-dashed border-gray-400 px-16 py-8">
       <div>LOGO</div>
       <div>
         <HeaderButton text="menu 1" />
