a6a9e5bdf98c3560a60365d63f998738befffcc1
dyi919 <ehddjs13579@gmail.com>
2023-02-09 00:09:39 +0900

    [style] add header scroll toggle effect

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
 import HeaderButton from './HeaderButton';
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setShow(window.scrollY < scrollY ? true : false);
+      setScrollY(window.scrollY);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollY]);
+
   return (
-    <div className="flex justify-between border-2 border-dashed border-gray-400 px-16 py-8">
+    <div
+      className={`fixed z-10 flex w-full justify-between border-2 border-dashed border-gray-400 px-16 py-8 transition duration-500 ease-in-out ${
+        show ? '' : 'transparent'
+      }`}
+    >
       <div>LOGO</div>
       <div>
         <HeaderButton text="menu 1" />
