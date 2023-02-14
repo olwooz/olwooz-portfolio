88f1997570d7fb52eacb9fbb41c79d1da20f57af
dyi919 <ehddjs13579@gmail.com>
2023-02-15 01:08:55 +0900

    [feature] implement slot machine effect

--- src/components/Contents/Main/Main.tsx
+++ src/components/Contents/Main/Main.tsx
@@ -1,12 +1,14 @@
 import ContentWrapper from '../ContentWrapper';
+import { textData } from './data';
+import SlotMachine from './SlotMachine';
 
 const Main = () => {
   return (
     <ContentWrapper style="flex items-center">
-      <div>
+      <div className="w-full">
         <h3 className="mb-6 text-2xl font-light">안녕하세요.</h3>
-        <h1 className="mb-4 text-9xl font-thin">무슨무슨 개발자</h1>
-        <h3 className="text-4xl font-black">누구누구입니다.</h3>
+        <SlotMachine textData={textData} />
+        <h3 className="mt-4 text-4xl font-black">누구누구입니다.</h3>
       </div>
     </ContentWrapper>
   );
---
+++ src/components/Contents/Main/data.ts
@@ -0,0 +1,1 @@
+export const textData = ['왕 멋진 개발자', '기가 막힌 개발자', '협업 대상 1순위 개발자', 'ChatGPT가 참고하는 개발자'];
