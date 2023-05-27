9c025d576e1f6a549e23e1a6472e8e7e3d8ecfdb
dyi919 <ehddjs13579@gmail.com>
2023-05-28 03:00:47 +0900

    [update] add wavve to project section

--- src/components/Contents/Projects/data.ts
+++ src/components/Contents/Projects/data.ts
@@ -3,6 +3,12 @@ import { Project } from '@Projects/types';
 export const projectData: Project[] = [
   {
     id: 1,
+    title: 'Wavve Tech Internship',
+    previewImage: '/images/preview_wavve.webp',
+    notion: 'https://olwooz.notion.site/Wavve-Tech-Internship-2023-03-06-2023-05-12-76e5d57d98d2474bba0a60cf8ef64381'
+  },
+  {
+    id: 2,
     title: 'Portfolio Web Page',
     previewImage: '/images/preview_portfolio_dark.gif',
     previewImageDarkMode: '/images/preview_portfolio.gif',
@@ -10,24 +16,23 @@ export const projectData: Project[] = [
     notion: 'https://olwooz.notion.site/2022-02-2022-04-d2779ca3abbd4cf6ae5e89b9d53ca6c3'
   },
   {
-    id: 2,
+    id: 3,
     title: 'Snowman Village',
     previewImage: '/images/preview_snowtown.webp',
     github: 'https://github.com/snowballers/snowball-frontend',
     notion: 'https://www.notion.so/olwooz/2022-12-2023-02-b52fc1bd1fe44e378c441f19caad4a4d?pvs=4',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Asyncrum',
     previewImage: '/images/preview_asyncrum.webp',
     github: 'https://github.com/Whiteboard-Journey/asyncrum-frontend',
     notion: 'https://www.notion.so/olwooz/Asyncrum-2022-05-2022-11-a734f516db904050bdb119fa47f7bfab?pvs=4',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Clean21 FW',
     previewImage: '/images/preview_clean21fw.webp',
     notion: 'https://olwooz.notion.site/Clean21-FW-2021-06-2022-01-1d00ccde2c8341d5a5dc42668514268c'
   },
-
 ];
