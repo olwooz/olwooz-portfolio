249d59e82a7b615e710cf0d1bb680a54ffdd8b81
dyi919 <ehddjs13579@gmail.com>
2023-06-17 19:45:12 +0900

    [feature] add vercel web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import { appWithTranslation } from 'next-i18next';
 import '@/styles/globals.css';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+};
 
 export default appWithTranslation(App);
