21723020a1d3fcc32d8bf41d35554f648bc0cb14
Dongun Yi <24418404+olwooz@users.noreply.github.com>
2024-05-11 01:56:10 +0900

    Update data.ts

--- src/components/Contents/Projects/data.ts
+++ src/components/Contents/Projects/data.ts
@@ -29,10 +29,4 @@ export const projectData: Project[] = [
     github: 'https://github.com/Whiteboard-Journey/asyncrum-frontend',
     notion: 'https://www.notion.so/olwooz/Asyncrum-2022-05-2022-11-a734f516db904050bdb119fa47f7bfab?pvs=4',
   },
-  {
-    id: 5,
-    title: 'Clean21 FW',
-    previewImage: '/images/preview_clean21fw.webp',
-    notion: 'https://olwooz.notion.site/Clean21-FW-2021-06-2022-01-1d00ccde2c8341d5a5dc42668514268c'
-  },
 ];
